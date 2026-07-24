d9841762ec788e231b1d2297e3c7d4a155a84055
David <122738904+kys928@users.noreply.github.com>
2026-07-24 14:01:55 +0200

    Export topology attachment API

--- src/triqto/phase15_6/__init__.py
+++ src/triqto/phase15_6/__init__.py
@@ -18,14 +18,24 @@ from .config import (
 )
 from .environment import inspect_phase156_environment
 from .planner import PHASE156_PLAN_SCHEMA, build_campaign_plan, resolve_config_path
+from .topology_attachment import (
+    ATTACHMENT_VERSION,
+    SCHEMA_VERSION as TOPOLOGY_ATTACHMENT_SCHEMA_VERSION,
+    TopologyAttachmentConfig,
+    attach_phase11_topology,
+)
 
 __all__ = [
+    "ATTACHMENT_VERSION",
     "DataBuildConfig",
     "PHASE156_CONFIG_SCHEMA",
     "PHASE156_PLAN_SCHEMA",
     "Phase156CampaignConfig",
     "PodRequirements",
+    "TOPOLOGY_ATTACHMENT_SCHEMA_VERSION",
+    "TopologyAttachmentConfig",
     "aggregate_campaign",
+    "attach_phase11_topology",
     "build_campaign_plan",
     "inspect_phase156_environment",
     "load_phase156_config",
