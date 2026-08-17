090a7f0aeda94420ea79b14c87cc20a04b5fc066
David <122738904+kys928@users.noreply.github.com>
2026-08-17 20:50:45 +0200

    Export Step 9B diagnostic acquisition API

--- src/triqto/hardware/__init__.py
+++ src/triqto/hardware/__init__.py
@@ -1,8 +1,38 @@
 """Hardware ingestion boundary helpers."""
+from .diagnostic_acquisition import (
+    BASIS_ORDER,
+    PROGRAM_ORDER,
+    HardwareDiagnosticAcquisition,
+    acquire_paired_diagnostics,
+    build_measurement_circuit,
+    build_paired_measurement_circuits,
+    build_step7_model_batch_from_counts,
+    compile_paired_measurement_circuits,
+    empirical_stats_from_counts,
+    make_ibm_runtime_sampler,
+    paired_diagnostic_arrays,
+)
 from .hardware_schema import FORBIDDEN_PHYSICAL_FIELDS, HardwareJobSpec, HardwareResultRecord
-from .ibm_runtime import RuntimeClient, RuntimeSubmissionError, collect_hardware_result, require_runtime_environment, submit_hardware_job
+from .ibm_runtime import (
+    RuntimeClient,
+    RuntimeSubmissionError,
+    collect_hardware_result,
+    require_runtime_environment,
+    submit_hardware_job,
+)
 
 __all__ = [
+    "BASIS_ORDER",
+    "PROGRAM_ORDER",
+    "HardwareDiagnosticAcquisition",
+    "acquire_paired_diagnostics",
+    "build_measurement_circuit",
+    "build_paired_measurement_circuits",
+    "build_step7_model_batch_from_counts",
+    "compile_paired_measurement_circuits",
+    "empirical_stats_from_counts",
+    "make_ibm_runtime_sampler",
+    "paired_diagnostic_arrays",
     "FORBIDDEN_PHYSICAL_FIELDS",
     "HardwareJobSpec",
     "HardwareResultRecord",
